29adf660c6c1e27b9edba9c93a84f540432241f2
ResistingGuide4 <thomasmrose11@gmail.com>
2025-11-29 15:45:28 -0500

    Initial Commit

---
+++ src/main/java/net/thomas/stuffmod/StuffMod.java
@@ -0,0 +1,16 @@
+package net.thomas.stuffmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StuffMod implements ModInitializer {
+	public static final String MOD_ID = "stuffmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/thomas/stuffmod/StuffModClient.java
@@ -0,0 +1,10 @@
+package net.thomas.stuffmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StuffModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
